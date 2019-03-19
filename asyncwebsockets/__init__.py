da68356a9aaa7adc263d2f37a3d25ae812e925a3
Matthias Urlichs <matthias@urlichs.de>
2019-03-19 09:09:42 +0100

    flake8'd

--- asyncwebsockets/__init__.py
+++ asyncwebsockets/__init__.py
@@ -3,3 +3,5 @@ asyncwebsockets - an asynchronous library for handling websockets.
 """
 
 from asyncwebsockets.client import open_websocket, create_websocket
+
+__all__ = ["open_websocket", "create_websocket"]
